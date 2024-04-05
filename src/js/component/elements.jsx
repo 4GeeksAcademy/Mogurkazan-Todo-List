88dbfc7839732d5015387ac08b76e3195d4eccb8
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-04-05 19:05:02 +0000

    estilos done

--- src/js/component/elements.jsx
+++ src/js/component/elements.jsx
@@ -6,34 +6,41 @@ import React, {useState} from "react";
 const Elementos = () => {
     const [texto, setTexto] = useState("");
     const [tareas, setTareas] = useState([]);
+    const [counter, setCounter] = useState(0);
     //mi segunda prueba
     const agregarTarea = (e) => {
         if (e.key === 'Enter' && texto !== '') {
             setTareas([...tareas, texto]);
             setTexto('');
+            setCounter(counter + 1);
         }
     };
     const eliminarTarea = (indice) => {
         const nuevasTareas = [...tareas];
         nuevasTareas.splice(indice, 1);
         setTareas(nuevasTareas);
+        setCounter(counter -1);
     }
     
     const handleChange = (e) => {
         setTexto(e.target.value);
     };
 	return (
-        <div className="caja container mt-5">
+        <div className="caja container d-flex flex-column justify-content-center text-center">
+            <h1>TAREAS</h1>
             <div className="carta card">
-                <h1>TAREAS</h1>
-                <input type="text" value={texto} onChange={handleChange} onKeyDown={agregarTarea} />
+                <input className="entrada text-start ps-5" type="text" value={texto} onChange={handleChange} onKeyDown={agregarTarea} />
                 
-                <ul>{tareas.map((item, index) => (
-                        <p key={index}>{item}
-                        <button className="btn-sm btn-secondary rounded-pill ms-auto" onClick={() => eliminarTarea(index)}>X</button></p>
+                <ul className="m-0 p-0" >{tareas.map((item, index) => (
+                    <div className="cajita d-flex">
+                        <p className="ps-5" key={index}>{item}
+                        </p>
+                        <button className="x btn-sm  rounded-pill ms-auto text-end" onClick={() => eliminarTarea(index)}>X</button>
+                    </div>
+                        
                     ))}
                 </ul>
-
+                <p className="pie text-start m-2">{counter} items left</p>
             </div>
         
         </div>
@@ -46,7 +53,3 @@ export default Elementos;
 //hacer una array y una función que convierta cada elemento de ese array en item de una lista
 //hacer botón para eliminar item de esa lista
 //dar formato a las listas y a la pagina
-//
-//
-//
-//
